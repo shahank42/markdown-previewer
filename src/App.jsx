6759411e537cc6bd193e4a416e16282ef7590cf8
Shounak Ghosh <crazykavlogs@gmail.com>
2023-01-25 23:55:52 +0530

    Stupid height bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="flex flex-col max-h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
       <Content />
       <Footer />
