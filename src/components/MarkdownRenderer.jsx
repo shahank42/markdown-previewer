99d45db29637f338d12c33bb91a6fc1e1082c72f
shahank42 <crazykavlogs@gmail.com>
2023-01-27 01:52:38 +0530

    Fixed text rendering Stupid workaround by overriding TailwindCSS class rules

--- src/components/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer.jsx
@@ -4,7 +4,7 @@ import ReactMarkdown from "react-markdown";
 const MarkdownRenderer = ({ rawMarkdown }) => {
     return (
         <div className="flex max-h-full">
-            <div className="bg-neutral-800 w-full py-4 px-6 prose dark:prose-invert prose-neutral max-w-none" id="preview">
+            <div className="bg-neutral-800 w-full py-4 px-6 prose !prose-invert prose-neutral max-w-none" id="preview">
                 {/* <Markdown className="overflow-y-scroll">{ rawMarkdown }</Markdown> */}
                 <ReactMarkdown>{ rawMarkdown }</ReactMarkdown>
             </div>
